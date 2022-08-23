9e83b7196a3265e3f1bed794fbb3215f49685536
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-08-22 20:46:04 -0700

    Update pass for on_message event subsystem

--- reference.py
+++ reference.py
@@ -1,6 +1,6 @@
 import discord
 
-startup_extensions = ["jishaku", "exec", "cpanel", "bot_management", "others", "ftx", "clap"]
+startup_extensions = ["jishaku", "on_message", "exec", "cpanel", "bot_management", "others", "ftx", "clap"]
 # startup_cogs = ["jishaku", "exec", "cpanel", "bot_management", "on_message", "on_voice_state_update", "ftx", "channel_logging", "others", "vc_management", "clap"]
 
 # Guilds
