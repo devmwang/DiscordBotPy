937c330bee287209ce66b75eeacb3a954d9ba4ce
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-01-29 12:55:04 -0800

    Initial chat filter

--- on_message.py
+++ on_message.py
@@ -14,22 +14,35 @@ class OnMessage(Cog):
     @Cog.listener()
     async def on_message(self, message):
         # Exec OS Level Commands with Sudo
-        if message.content[0:4] == "sudo" and message.author.bot == False:            
+        if message.content[0:4] == "sudo" and message.author.bot == False:
             if message.author.id == self.client.admin_id:
                 try:
-                    pipe = subprocess.Popen(message.content, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    pipe = subprocess.Popen(
+                        message.content,
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                    )
                     out, err = pipe.communicate()
                     error = err.decode()
                     response = out.decode()
                     combined = response + error
                     if combined == "":
-                        await message.reply(f"{self.client.checkmarkGlyph(message.guild)} Command executed with no output.")
+                        await message.reply(
+                            f"{self.client.checkmarkGlyph(message.guild)} Command executed with no output."
+                        )
                     else:
                         await message.reply(f"```{combined}```")
                 except discord.errors.HTTPException as _e:
                     await message.reply(self.client.xmarkGlyph(message.guild), _e)
             else:
-                await message.reply(f"You do not have permission to use this command. {self.client.xmarkGlyph(message.guild)}")
+                await message.reply(
+                    f"You do not have permission to use this command. {self.client.xmarkGlyph(message.guild)}"
+                )
+
+        # Chat Filter
+        if message.content.contains("Did you mean "):
+            message.delete()
 
 
 # Setup & Link
