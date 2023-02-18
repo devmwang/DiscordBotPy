c9c4b7210488e6ca29cf30f7147517fb7f94896a
Michael Wang <mwang60@students.cs.ubc.ca>
2023-02-17 17:17:36 -0800

    Updated regex for update module matching

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def main():
         if "Already up to date." in response:
             pass  # Do nothing
         elif "fatal" not in combined:
-            updated_modules = set(re.findall(r"(\w+\.py)", combined))
+            updated_modules = set(re.findall(r"(\w+(?=\.py))", combined))
 
             await interaction.followup.send(
                 "Successfully pulled updates from Github. Attempting to restart modified modules."
